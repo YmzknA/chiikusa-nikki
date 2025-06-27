de40e8cbdd01e4da0bfe81250abeb564dc11fdd0
YmzknA <taiyo.yamazaki.35b@gmail.com>
2025-06-28 00:32:47 +0900

    fix: lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,14 +16,14 @@ class ApplicationController < ActionController::Base
   end
 
   def unauthenticated_user
-    redirect_to root_path, alert: 'ログインが必要です'
+    redirect_to root_path, alert: "ログインが必要です"
   end
 
   private
 
   def restrict_devise_routes
-    if devise_controller? && action_name.in?(['new', 'create']) && controller_name.in?(['sessions', 'registrations'])
-      redirect_to root_path, alert: 'この機能は利用できません'
+    if devise_controller? && action_name.in?(%w[new create]) && controller_name.in?(%w[sessions registrations])
+      redirect_to root_path, alert: "この機能は利用できません"
     end
   end
 end
